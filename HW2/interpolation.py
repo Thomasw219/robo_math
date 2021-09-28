63eae1a30ddd351bd8af19612a994cbacd8c021f
Thomas Wei <thomasw219@gmail.com>
2021-09-28 11:40:38 -0400

    problem 1 done

--- HW2/interpolation.py
+++ HW2/interpolation.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def compute_coefficients(x, y):
     n = len(x)
@@ -23,4 +24,67 @@ def divided_differences_interpolation(x, y, x_eval):
         y_eval += term
     return y_eval
 
+# 1b computation and graph
+print("1b")
+x = np.array([0, 1/8, 1/4, 1/2, 3/4, 1])
+y = np.exp(-1 / 2 * np.square(x))
 
+x_eval = np.array([1 / 3])
+y_eval = divided_differences_interpolation(x, y, x_eval)
+
+print("x: {}\ny: {}".format(x_eval, y_eval))
+
+plt.scatter(x, y, c='b', label='Given')
+plt.scatter(x_eval, y_eval, c='g', label='Interpolated Value(s)')
+plt.legend()
+plt.savefig("./1b.png")
+
+# 1c computation and graphs
+print("1c")
+x_eval = np.array([0.06])
+for n in [2, 4, 40]:
+    x = np.array([i * 2 / n - 1 for i in range(n + 1)])
+    y = np.array(np.divide(1, np.add(1, 36 * np.square(x))))
+    y_eval = divided_differences_interpolation(x, y, x_eval)
+
+    print("x: {}\ny: {}".format(x_eval, y_eval))
+
+    plt.clf()
+    plt.scatter(x, y, c='b', label='Given')
+    plt.scatter(x_eval, y_eval, c='g', label='Interpolated Value(s)')
+    plt.legend()
+    plt.savefig("./1c_n={}.png".format(n))
+
+print("Actual y value: {}".format(1 / (1 + 36 * 0.006**2)))
+
+# 1d computation and graphs
+print("1d")
+x_eval = np.linspace(-1, 1, num=1000)
+
+max_errors = []
+n_values = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 40]
+for n in n_values:
+    x = np.array([i * 2 / n - 1 for i in range(n + 1)])
+    y = np.array(np.divide(1, np.add(1, 36 * np.square(x))))
+
+    y_real = np.array(np.divide(1, np.add(1, 36 * np.square(x_eval))))
+    y_eval = divided_differences_interpolation(x, y, x_eval)
+    pointwise_errors = np.abs(y_real - y_eval)
+    max_error = np.max(pointwise_errors)
+    max_errors.append(max_error)
+
+    plt.clf()
+    plt.ylim(-1, 1)
+    plt.xlim(-1, 1)
+    plt.plot(x_eval, [0 for i in range(len(x_eval))], c='k')
+    plt.scatter(x, y, c='b', label='Given')
+    plt.plot(x_eval, y_eval, c='g', label='Interpolated Value(s)')
+    plt.plot(x_eval, y_real, c='b', label='Real Values')
+    plt.plot(x_eval, pointwise_errors, c='r', label='Error')
+    plt.legend()
+    plt.savefig("./1d_n={}.png".format(n))
+
+print("n values: {}\nErrors: {}".format(n_values, max_errors))
+plt.clf()
+plt.scatter(n_values, np.log(max_errors))
+plt.savefig("./1d_errors_by_n.png")
