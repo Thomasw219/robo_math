4ff71a66b26dcb1c3fdacdb5d97a6febaaaff881
Thomas Wei <thomasw219@gmail.com>
2021-10-14 20:14:45 -0400

    finish ransac, need to tune for cluttered hallway

--- HW3/approximation.py
+++ HW3/approximation.py
@@ -60,6 +60,7 @@ def plot_planes_and_points(name, planes, plane_points, other_points, x_range, y_
 
     if other_points is not None:
         ax.scatter(other_points[:, 0], other_points[:, 1], other_points[:, 2], color='r', s=2)
+    ax.view_init(80, -30)
     plt.savefig(name)
 
 def get_ranges(X):
@@ -80,18 +81,39 @@ def partition_points(X, distances, threshold):
 
 THRESH = 0.01
 def regress_process(X, points):
-    beta = linear_regression(points, -1 * np.ones((points.shape[0], 1)))
+    beta = linear_regression(X, -1 * np.ones((X.shape[0], 1)))
     plane_params = np.concatenate([beta.flatten(), [1]])
     distances = get_distances(points, plane_params)
     plane_points, other_points = partition_points(points, distances, 0.01)
     return plane_params, plane_points, other_points, distances
 
 def RANSAC(points, num_planes, num_samples, sample_size):
+    planes = []
+    all_plane_points = []
+    other_points = None
+    remaining_points = points
     for i in range(num_planes):
+        print("Plane: {}".format(i + 1))
+        max_points_fit = 0
+        max_plane_params = None
+        max_plane_points = None
+        max_other_points = None
         for j in range(num_samples):
-            points_subset = np.random.choice(points, size=sample_size)
+            points_subset = remaining_points[np.random.choice(remaining_points.shape[0], size=sample_size, replace=False)]
+            plane_params, plane_points, other_points, distances = regress_process(points_subset, remaining_points)
+            if plane_points.shape[0] > max_points_fit:
+                print("Fit points: {}".format(plane_points.shape[0]))
+                max_points_fit = plane_points.shape[0]
+                max_plane_params = plane_params
+                max_plane_points = plane_points
+                max_other_points = other_points
+        planes.append(max_plane_params)
+        all_plane_points.append(max_plane_points)
+        remaining_points = max_other_points
+    return planes, np.concatenate(all_plane_points), remaining_points
 
 def q4():
+    """
     clear_table_points = read_point_cloud_file("data/clear_table.txt")
     plane_params, plane_points, other_points, distances = regress_process(clear_table_points, clear_table_points)
     ranges = get_ranges(clear_table_points)
@@ -104,7 +126,26 @@ def q4():
     ranges = get_ranges(cluttered_table_points)
     plot_planes_and_points("figures/cluttered_table.png", [plane_params], plane_points, other_points, *ranges)
     print("Cluttered table fit average distance: {}".format(np.mean(distances)))
-    print("Points within threshold of {} from plane(s): {}/{}".format(THRESH, plane_points.shape[0], clear_table_points.shape[0]))
+    print("Points within threshold of {} from plane(s): {}/{}".format(THRESH, plane_points.shape[0], cluttered_table_points.shape[0]))
+
+    plane_params, plane_points, other_points = RANSAC(cluttered_table_points, 1, 50, 20)
+    ranges = get_ranges(cluttered_table_points)
+    plot_planes_and_points("figures/cluttered_table_RANSAC.png", plane_params, plane_points, other_points, *ranges)
+    print("Cluttered table RANSAC points within threshold of {} from plane(s): {}/{}".format(THRESH, plane_points.shape[0], cluttered_table_points.shape[0]))
+
+    clean_hallway_points = read_point_cloud_file("data/clean_hallway.txt")
+    plane_params, plane_points, other_points = RANSAC(clean_hallway_points, 4, 200, 4)
+    ranges = get_ranges(clean_hallway_points)
+    plot_planes_and_points("figures/clean_hallway_RANSAC.png", plane_params, plane_points, other_points, *ranges)
+    print("Clean hallway RANSAC points within threshold of {} from plane(s): {}/{}".format(THRESH, plane_points.shape[0], clean_hallway_points.shape[0]))
+    """
+
+    cluttered_hallway_points = read_point_cloud_file("data/cluttered_hallway.txt")
+    plane_params, plane_points, other_points = RANSAC(cluttered_hallway_points, 4, 500, 4)
+    ranges = get_ranges(cluttered_hallway_points)
+    plot_planes_and_points("figures/cluttered_hallway_RANSAC.png", plane_params, plane_points, other_points, *ranges)
+    print("Clean hallway RANSAC points within threshold of {} from plane(s): {}/{}".format(THRESH, plane_points.shape[0], cluttered_hallway_points.shape[0]))
+
 
 #q1()
 #q2()
